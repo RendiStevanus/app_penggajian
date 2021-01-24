2c375c0c9fd72f16aa175bfc20c1096186efdbfd
RendyStepanusS <rendivanbeck@gmail.com>
2021-01-24 22:53:03 +0700

    masih salah

--- application/controllers/admin/DataPegawai.php
+++ application/controllers/admin/DataPegawai.php
@@ -11,4 +11,77 @@ class DataPegawai extends CI_Controller
     $this->load->view('admin/dataPegawai', $data);
     $this->load->view('templates_admin/footer');
   }
+
+  public function tambahData()
+  {
+    $data['judul'] = 'Tambah Data Pegawai';
+
+    // mengambil data dari table data_jabatan
+    $data['jabatan'] = $this->penggajianModel->getData('data_jabatan')->result();
+
+    //Load view
+    $this->load->view('templates_admin/header', $data);
+    $this->load->view('templates_admin/sidebar');
+    $this->load->view('admin/tambahDataPegawai', $data);
+    $this->load->view('templates_admin/footer');
+  }
+  public function tambahDataAksi()
+  {
+    $this->_rules();
+
+    if ($this->form_validation->run() == FALSE) {
+      $this->tambahData();
+    } else {
+      $nik            = $this->input->post('nik');
+      $nama_pegawai   = $this->input->post('nama_pegawai');
+      $jenis_kelamin  = $this->input->post('jenis_kelamin');
+      $tanggal_masuk  = $this->input->post('tanggal_masuk');
+      $jabatan        = $this->input->post('jabatan');
+      $status         = $this->input->post('status');
+      $photo          = $_FILES['photo']['name'];
+      if ($photo = '') {
+      } else {
+        $config['upload_path']   = '.assets/photo';
+        $config['allowed_types'] = 'jpg|jpeg|png|tiff';
+        $this->load->library('upload', $config);
+        if (!$this->upload->do_upload('photo')) {
+          echo "Photo Gagal Diupload!";
+        } else {
+          $photo = $this->upload->data('file_name');
+        }
+      }
+
+      $data = array(
+        'nik'           => $nik,
+        'nama_pegawai'  => $nama_pegawai,
+        'jenis_kelamin' => $jenis_kelamin,
+        'jabatan'       => $jabatan,
+        'tanggal_masuk' => $tanggal_masuk,
+        'status'        => $status,
+        'photo'         => $photo
+      );
+
+      $this->penggajianModel->insert_data($data, 'data_pegawai');
+      $this->session->set_flashdata(
+        'pesan',
+        '<div class="alert alert-success alert-dismissible fade show" role="alert">
+            <strong>Data berhasil ditambahkan!</strong>
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+            </button>
+        </div>'
+      );
+
+      redirect('admin/DataPegawai');
+    }
+  }
+  public function _rules()
+  {
+    $this->form_validation->set_rules('nik', 'NIK', 'required');
+    $this->form_validation->set_rules('nama_pegawai', 'nama pegawai', 'required');
+    $this->form_validation->set_rules('jenis_kelamin', 'jenis kelamin', 'required');
+    $this->form_validation->set_rules('jabatan', 'jabatan', 'required');
+    $this->form_validation->set_rules('tanggal_masuk', 'tanggal masuk', 'required');
+    $this->form_validation->set_rules('status', 'status', 'required');
+  }
 }
---
+++ application/views/admin/tambahDataPegawai.php
@@ -0,0 +1,76 @@
+<div class="container-fluid">
+  <!-- Title -->
+  <div class="d-sm-flex align-items-center justify-content-between mb-4">
+    <h1 class="h3 mb-0 text-gray-800"><?= $judul; ?></h1>
+  </div>
+  <!-- End Title -->
+
+  <!-- Main Content -->
+  <div class="card" style="width: 60%;">
+    <div class="card-body">
+      <form action="<?= base_url('admin/DataPegawai/tambahDataAksi') ?>" method="POST" enctype="multiprt/form-data">
+
+        <div class="form-group">
+          <label>NIK</label>
+          <input type="number" name="nik" class="form-control">
+          <?= form_error('nik', '<div class="text-small text-danger"></div>') ?>
+        </div>
+
+        <div class="form-group">
+          <label>Nama Pegawai</label>
+          <input type="text" name="nama_pegawai" class="form-control">
+          <?= form_error('nama_pegawai', '<div class="text-small text-danger"></div>') ?>
+        </div>
+
+        <div class="form-group">
+          <label>Jenis Kelamin</label>
+          <select name="jenis_kelamin" class="form-control">
+            <option>-- Pilih Jenis Kelamin --</option>
+            <option value="Laki-laki">Laki-Laki</option>
+            <option value="Perempuan">Perempuan</option>
+          </select>
+          <?= form_error('jenis_kelamin', '<div class="text-small text-danger"></div>') ?>
+        </div>
+
+        <!-- Pengulangan data jabatan -->
+        <div class="form-group">
+          <label>Jabatan</label>
+          <select name="jabatan" class="form-control">
+            <option>-- Pilih Jabatan --</option>
+            <?php foreach ($jabatan as $j) : ?>
+              <option value="<?= $j->nama_jabatan; ?>"><?= $j->nama_jabatan; ?></option>
+            <?php endforeach; ?>
+          </select>
+          <?= form_error('jabatan', '<div class="text-small text-danger"></div>') ?>
+        </div>
+        <!-- end -->
+
+        <div class="form-group">
+          <label>Tanggal Masuk</label>
+          <input type="date" name="tanggal_masuk" class="form-control">
+          <?= form_error('tanggal_masuk', '<div class="text-small text-danger"></div>') ?>
+        </div>
+
+        <div class="form-group">
+          <label>Status Pegawai</label>
+          <select name="jabatan" class="form-control">
+            <option>-- Pilih Status Pegawai --</option>
+            <option value="Pegawai Tetap">Pegawai Tetap</option>
+            <option value="Pegawai Tidak Tetap">Pegawai Tidak Tetap</option>
+          </select>
+          <?= form_error('status', '<div class="text-small text-danger"></div>') ?>
+        </div>
+
+        <div class="form-group">
+          <label>Photo</label>
+          <input type="file" name="photo" class="form-control">
+        </div>
+
+        <button type="submit" class="btn btn-primary">Simpan</button>
+
+      </form>
+    </div>
+  </div>
+  <!-- End Main Content -->
+
+</div>
